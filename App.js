cdcab85cb7ead75d816ee279582070b7d65d8452
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-17 11:48:58 -0800

    Add import google fonts

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+//import fonts
+import { useFonts } from 'expo-font';
+import { 
+    DancingScript_400Regular,
+    DancingScript_500Medium,
+    DancingScript_600SemiBold,
+    DancingScript_700Bold 
+  } from '@expo-google-fonts/dancing-script'
+  
 
 export default function App() {
+    let [fontsLoaded, error] = useFonts({
+        Regular: DancingScript_400Regular,
+        Medium: DancingScript_500Medium,
+        SemiBold: DancingScript_600SemiBold,
+        Bold: DancingScript_700Bold,
+        'Bakbak-One': require('./assets/fonts/BakbakOne-Regular.ttf'),
+        'KaushanScript': require('./assets/fonts/KaushanScript-Regular.ttf'),
+        'Playfair-Italic': require('./assets/fonts/PlayfairDisplay-Italic-VariableFont_wght.ttf')
+    });
+if (!fontsLoaded) {
+    return <AppLoading />
+}
+
   return (
     <View style={styles.container}>
-      <Text>Sign-in</Text>
+      <Text 
+            style={{ 
+            fontFamily: 'Playfair-Italic', 
+            fontSize: 60 }}
+        >Sign-in
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
